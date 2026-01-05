1e61d8c8777957241780e49b659ac91acf4edf4a
destroyer1951 <liammichealsipes@gmail.com>
2026-01-05 16:53:49 -0700

    separate files for prices and items

---
+++ scripts/items.js
@@ -0,0 +1,156 @@
+import { world, system, ItemStack } from '@minecraft/server'
+
+export function makeItem(typeId, configure) { // i wrote this code and i dont even understand it anymore am i the best developer of all time
+  const item = new ItemStack(typeId)
+  configure(item)
+  return item
+}
+
+export function rollStars(maxStars=5) {
+    let stars = 1
+    let starString = "§l"
+
+    for (let i = 2; i <= maxStars; i++) {
+        if (Math.random() < 1 / i) {
+            stars++
+        } else {
+            break
+        }
+  }
+
+    for (let i = stars; i > 0; i--) {
+        starString += "* "
+    }
+
+    return starString.trim()
+}
+
+export const items = {}
+system.run(() => {
+    // General shop items
+    items.lavaBucket = makeItem("minecraft:lava_bucket", item => {
+        item.nameTag = "§r§fLava Bucket"
+    })
+    items.ice = makeItem("minecraft:ice", item => {
+        item.nameTag = "§r§fIce"
+    })
+    items.oakLog = makeItem("minecraft:oak_log", item => {
+        item.nameTag = "§r§fOak Log"
+    })
+    items.grassBlock = makeItem("minecraft:grass_block", item => {
+    item.nameTag = "§r§fGrass Block"
+    })
+    items.dirt = makeItem("minecraft:dirt", item => {
+        item.nameTag = "§r§fDirt"
+    })
+    items.cobblestone = makeItem("minecraft:cobblestone", item => {
+        item.nameTag = "§r§fCobblestone"
+    })
+    items.sand = makeItem("minecraft:sand", item => {
+        item.nameTag = "§r§fSand"
+    })
+    items.boneMeal = makeItem("minecraft:bone_meal", item => {
+        item.nameTag = "§r§fBone Meal"
+    })
+    items.charcoal = makeItem("minecraft:charcoal", item => {
+        item.nameTag = "§r§fCharcoal"
+    })
+    items.oakSapling = makeItem("minecraft:oak_sapling", item => {
+        item.nameTag = "§r§fOak Sapling"
+    })
+    items.darkOakSapling = makeItem("minecraft:dark_oak_sapling", item => {
+        item.nameTag = "§r§fDark Oak Sapling"
+    })
+    items.darkOakLog = makeItem("minecraft:dark_oak_log", item => {
+        item.nameTag = "§r§fDark Oak Log"
+    })
+    items.birchSapling = makeItem("minecraft:birch_sapling", item => {
+        item.nameTag = "§r§fBirch Sapling"
+    })
+    items.birchLog = makeItem("minecraft:birch_log", item => {
+        item.nameTag = "§r§fBirch Log"
+    })
+    items.coal = makeItem("minecraft:coal", item => {
+        item.nameTag = "§r§fCoal"
+    })
+    items.copperIngot = makeItem("minecraft:copper_ingot", item => {
+        item.nameTag = "§r§fCopper Ingot"
+    })
+    items.ironIngot = makeItem("minecraft:iron_ingot", item => {
+        item.nameTag = "§r§fIron Ingot"
+    })
+    items.goldIngot = makeItem("minecraft:gold_ingot", item => {
+        item.nameTag = "§r§fGold Ingot"
+    })
+    items.diamond = makeItem("minecraft:diamond", item => {
+        item.nameTag = "§r§fDiamond"
+    })
+    items.quartzCrystal = makeItem("minecraft:quartz", item => {
+        item.nameTag = "§r§fQuartz Crystal"
+        item.setLore(["", "§r§8Both exceptionally shiny", "§r§8and exceptionally sharp", "", `§r§e${rollStars()}`])
+    })
+
+    Object.defineProperty(items, "padparadscha", {
+        get() {
+            return makeItem("minecraft:resin_brick", item => {
+                item.nameTag = "§r§fPadparadscha"
+                item.setLore(["", "§r§8A powerful gemstone,", "§r§8in difficulty and durability", "", `§r§e${rollStars()}`])
+            })
+        }
+    })
+
+    // Farm shop items
+
+    items.wheat = makeItem("minecraft:wheat", item => {
+        item.nameTag = "§r§fWheat"
+    })
+    items.wheatSeeds = makeItem("minecraft:wheat_seeds", item => {
+        item.nameTag = "§r§fWheat Seeds"
+    })
+    items.potato = makeItem("minecraft:potato", item => {
+        item.nameTag = "§r§fPotato"
+    })
+    items.sugarCane = makeItem("minecraft:sugar_cane", item => {
+        item.nameTag = "§r§fSugar Cane"
+    })
+
+    // Fishing shop items
+
+    Object.defineProperty(items, "basicRod", {
+        get() {
+            return makeItem("minecraft:fishing_rod", item => {
+                item.nameTag = "§r§fBasic Fishing Rod"
+                item.setLore(["", "§r§8It's not much but", "it gets the job done", '', `§r§e${rollStars()}`])
+            })
+        }
+    })
+
+    items.rawCod = makeItem("minecraft:cod", item => {
+        item.nameTag = "§r§fRaw Cod"
+    })
+    items.rawSalmon = makeItem("minecraft:salmon", item => {
+        item.nameTag = "§r§fRaw Salmon"
+    })
+    items.tropicalFish = makeItem("minecraft:tropical_fish", item => {
+        item.nameTag = "§r§fTropical Fish"
+    })
+    items.inkSac = makeItem("minecraft:ink_sac", item => {
+        item.nameTag = "§r§fInk Sac"
+    })
+    items.cherrySapling = makeItem("minecraft:cherry_sapling", item => {
+        item.nameTag = "§r§fCherry Sapling"
+    })
+    items.cherryLog = makeItem("minecraft:cherry_log", item => {
+        item.nameTag = "§r§fCherry Log"
+    })
+
+    Object.defineProperty(items, "prismarineShard", {
+        get() {
+            return makeItem("minecraft:prismarine_shard", item => {
+                item.nameTag = "§r§fPrismarine Shard"
+                item.setLore([`§r§e${rollStars()}`])
+            })
+        }
+    })
+
+})
---
+++ scripts/prices.js
@@ -0,0 +1,108 @@
+import { world, system, ItemStack } from '@minecraft/server'
+
+export const prices = {buy: {}, sell: {}}
+
+// General Shop Items
+
+prices.buy.lavaBucket = 1000
+prices.sell.lavaBucket = 100
+
+prices.buy.ice = 200
+prices.sell.ice = 10
+
+prices.buy.grassBlock = 1250
+prices.sell.grassBlock = 10
+
+prices.buy.dirt = 1000
+prices.sell.dirt = 50
+
+prices.buy.cobblestone = 15
+prices.sell.cobblestone = 1
+
+prices.buy.sand = 850
+prices.sell.sand = 10
+
+prices.buy.boneMeal = 100
+prices.sell.boneMeal = 10
+
+prices.buy.charcoal = 100
+prices.sell.charcoal = 12
+
+prices.buy.oakSapling = 1000
+prices.sell.oakSapling = 5
+
+prices.buy.oakLog = 200
+prices.sell.oakLog = 10
+
+prices.buy.darkOakSapling = 2500
+prices.sell.darkOakSapling = 5
+
+prices.buy.darkOakLog = 200
+prices.sell.darkOakLog = 8
+
+prices.buy.birchSapling = 1750
+prices.sell.birchSapling = 10
+
+prices.buy.birchLog = 200
+prices.sell.birchLog = 10
+
+prices.buy.coal = 100
+prices.sell.coal = 12
+
+prices.buy.copperIngot = 3000
+prices.sell.copperIngot = 20
+
+prices.buy.ironIngot = 10000
+prices.sell.ironIngot = 35
+
+prices.buy.goldIngot = 25000
+prices.sell.goldIngot = 50
+
+prices.buy.diamond = "§cN/A"
+prices.sell.diamond = 1
+
+prices.buy.quartzCrystal = "§cN/A"
+prices.sell.quartzCrystal = 1
+
+prices.buy.padparadscha = "§cN/A"
+prices.sell.padparadscha = 1
+
+// Farm shop items
+
+prices.buy.wheat = 50
+prices.sell.wheat = 6
+
+prices.buy.wheatSeeds = 500
+prices.sell.wheatSeeds = 1
+
+prices.buy.potato = 1000
+prices.sell.potato = 3
+
+prices.buy.sugarCane = 5000
+prices.sell.sugarCane = 5
+
+// Fishing shop items
+
+prices.buy.basicRod = 250
+prices.sell.basicRod = 0
+
+prices.buy.rawCod = 50
+prices.sell.rawCod = 10
+
+prices.buy.rawSalmon = 50
+prices.sell.rawSalmon = 12
+
+prices.buy.tropicalFish = 100
+prices.sell.tropicalFish = 20
+
+prices.buy.inkSac = 100
+prices.sell.inkSac = 45
+
+prices.buy.cherrySapling = "§cN/A"
+prices.sell.cherrySapling = 10
+
+prices.buy.cherryLog = "§cN/A"
+prices.sell.cherryLog = 12
+
+prices.buy.prismarineShard = "§cN/A"
+prices.sell.prismarineShard = 500
