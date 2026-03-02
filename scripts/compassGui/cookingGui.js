39d752c74580e9521f9199bec25f21882642c247
destroyer1951 <liammichealsipes@gmail.com>
2026-03-01 22:58:25 -0700

    cooking progress

--- scripts/compassGui/cookingGui.js
+++ scripts/compassGui/cookingGui.js
@@ -7,7 +7,7 @@ import { prices } from '../prices.js'
 import { getPlayerDynamicProperty, setPlayerDynamicProperty, getGlobalDynamicProperty, setGlobalDynamicProperty, getScore, setScore, setStat } from '../stats.js'
 import { checkItemAmount, checkInvEmpty, clearItem, getFreeSlots, rollWeightedItem, xpRequirements, achieve } from '../index.js'
 
-function cookingInfoMenu(player, item, recipe) {
+function cookingInfoMenu(player, item, recipe, minutes, usage) {
     let cleanName 
         if (item.nameTag) { 
             cleanName = item.nameTag.replace(/§./g, "")
@@ -22,21 +22,26 @@ function cookingInfoMenu(player, item, recipe) {
             lore.push(...["","§r§8Star count is randomized!"])
         }
 
+        recipe.forEach((line, i, arr) => {
+            if (arr[i] !== "") arr[i] = "§a - §7" + line
+        })
+
     const menu = new ChestFormData("45")
         menu.title(cleanName)
         
-        menu.button(20, `${item.nameTag || item.typeId}`, lore, item.typeId)
-        menu.button(22, "§6Cook This Item!", [], 'minecraft:campfire')
-
-        menu.button(15, 'Recipe:', recipe, 'minecraft:redstone')
-        menu.button(24, '', [], 'minecraft:lava_bucket')
-        menu.button(33, "Fuel", [], 'minecraft:coal')
-
-        menu.pattern(["xxxxxxxxx", // "glass_orange.png"
+        menu.pattern(["xxxxxxxxx", 
                      ".........",
                      ".........",
                      ".........",
                      "xxxxxxxxx"], {x: {itemName: '', itemDesc: [], texture: "textures/blocks/glass_orange"}})
+
+        menu.button(20, `${item.nameTag || item.typeId}`, lore, item.typeId)
+        menu.button(22, "§6Start Cooking!", ["", `§fCooking Time: §a${minutes}m`, `§fFuel Usage: §6${usage}`], 'minecraft:campfire')
+
+        menu.button(15, 'Recipe:', recipe, 'minecraft:redstone')
+        menu.button(24, "§6Add Fuel", [], "minecraft:lava_bucket")
+        menu.button(33, `§fFuel: §6${getPlayerDynamicProperty(player, "campfireFuel") || 0}§e/§6500`, [], "minecraft:coal")
+
 
         menu.show(player).then(a => {
             if (a.canceled) return
@@ -51,21 +56,27 @@ export function campfireCookingMenu(player) {
     const menu = new ChestFormData("54")
         .title("Campfire Cooking")
 
-        .pattern(["xxxxxxxxx",
+        .pattern(["xxxx.xxxx",
                 ".........",
                 ".........",
                 ".........",
                 ".........",
                 "xxxxxxxxx"], {x: {itemName: '', itemDesc: [], texture: "textures/blocks/glass_gray"}})
+        .button(4, "§aCampfire Cooking", ["", "§r§7Cook items and consume", "§7them to receive", "temporary stat buffs!", "", "You may only cook one", "item at a time, even", "with multiple campfires!"], "minecraft:emerald")
+        
         
         .button(10, "Candied Apple", ["", "§r§7View Recipe"], "minecraft:apple")
+
+
+        .button(25, "§6Add Fuel", [], "minecraft:lava_bucket")
+        .button(34, `§fFuel: §6${getPlayerDynamicProperty(player, "campfireFuel") || 0}§e/§6500`, [], "minecraft:coal")
 
         .show(player).then(a => {
             if (a.canceled) return
 
             switch (a.selection) {
                 case 10: {
-                    return cookingInfoMenu(player, items.candiedApple, ["not telling"])
+                    return cookingInfoMenu(player, items.candiedApple, ["4x Apple", "16x Sugar"], 20, 20)
                 }
             }
         })
